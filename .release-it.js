01c8cbd51279b6b0203df4f0a7c6d9c3b27e819a
Wayne Van Son <waynevanson@gmail.com>
2023-10-12 07:32:23 +1100

    chore: replace semantic release with release it

---
+++ .release-it.js
@@ -0,0 +1,25 @@
+const version = "${version}"
+
+module.exports = {
+  plugins: {
+    "@release-it/conventional-changelog": {
+      path: ".",
+      infile: "CHANGELOG.md",
+      preset: "conventionalcommits",
+    },
+  },
+  git: {
+    push: true,
+    tagName: `${version}`,
+    pushRepo: "git@github.com:waynevanson/generator.git",
+    commitMessage: `chore: released version ${version} [no ci]`,
+    requireCommits: true,
+  },
+  npm: {
+    publish: true,
+  },
+  github: {
+    release: true,
+    releaseName: `${version}`,
+  },
+}
